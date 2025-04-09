df4a9d7a0b95945015561157c79058652c903597
Simon Kovačič <simon.kovacic@qinshift.com>
2025-04-09 18:37:07 +0200

    fix: esm - .js, commonjs - .cjs

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -14,5 +14,6 @@ export default defineConfig({
   target: 'esnext',
   outDir: 'dist',
   platform: 'neutral',
+  tsconfig: 'tsconfig.json',
   external: ['fs', 'path', 'node:*', 'reflect-metadata', 'inversify', 'ms'],
 });
