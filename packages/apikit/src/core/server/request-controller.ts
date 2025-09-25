33b524d5f530b1a5dbff8db65660ee0d9e23ccba
Simon Kovačič <simon.kovacic@avenga.com>
2025-09-25 20:32:46 +0200

    fix: Apikit - remove unncessary RequestWithParams extending fastify request causing conflicts in signature

--- packages/apikit/src/core/server/request-controller.ts
+++ packages/apikit/src/core/server/request-controller.ts
@@ -13,8 +13,6 @@ import { type HttpResponse } from '#/http';
 type HttpReplyPromise = (request: FastifyRequest, options: object) => Promise<HttpResponse>;
 type HttpOrVoidReplyPromise = (request: FastifyRequest, options?: object) => Promise<HttpResponse | void>;
 
-export type RequestWithParams<TParams extends Record<string, string>> = FastifyRequest<{ Params: TParams }>;
-
 export interface RequestController {
   handler(
     controllerFn: HttpReplyPromise,
