bd02eb3246adc75b84041e1f71ed2de0a21d0950
Simon Kovačič <simonrastislavkovacic@gmail.com>
2025-12-28 12:36:11 +0100

    fix(apikt): rename HttpHeaders types

--- packages/apikit/src/core/http/http-headers.ts
+++ packages/apikit/src/core/http/http-headers.ts
@@ -2,6 +2,8 @@ import type { IncomingHttpHeaders } from 'http';
 
 export interface HttpHeaders extends IncomingHttpHeaders {}
 
-export type HttpHeadersKeys = {
+export type HttpHeaderValues = {
   [K in keyof HttpHeaders as string extends K ? never : number extends K ? never : K]: HttpHeaders[K];
 };
+
+export type HttpHeaderKey = Extract<keyof HttpHeaderValues, string>;
